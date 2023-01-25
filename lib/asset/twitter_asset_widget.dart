d3e348315c8cc20d3ad2cd5ee1fd9be064380773
Ethan-C-J <ecluculent@gmail.com>
2023-01-25 14:34:41 +1000

    Created basic Twitter API call

--- lib/asset/twitter_asset_widget.dart
+++ lib/asset/twitter_asset_widget.dart
@@ -1,5 +1,37 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart' as http;
+
+Future<String> _getTwitterData(String username, List<String> fields) async {
+  String apiUrl =
+      "https://api.twitter.com/2/users/by?usernames=${username.toLowerCase()}&user.fields=${fields.join(",")}";
+
+  // Require our own CORS wrapper to use Twitter API directly, along with extra
+  // security since this exposes API keys. This is only for development purposes
+  // and should not be used in production. Everything making use of API keys must
+  // be handled by middleware and on the back end and not on the front end like we
+  // are doing here.
+  String corsWrapperUrl = "https://cors-anywhere.herokuapp.com/";
+
+  final http.Response res = await http.get(
+    Uri.parse("$corsWrapperUrl$apiUrl"),
+    headers: {
+      'Authorization': 'Bearer YOUR_KEY%3DHERE',
+    },
+  );
+
+  if (res.statusCode == 404) {
+    return "User could not be found";
+  }
+
+  if (res.statusCode == 401 || res.statusCode == 403) {
+    return "Application is not authenticated";
+  }
+
+  return res.body;
+}
 
 class TwitterAssetWidget extends ConsumerWidget {
   @override
@@ -10,7 +42,7 @@ class TwitterAssetWidget extends ConsumerWidget {
       trailing: IconButton(
         icon: Icon(Icons.refresh),
         onPressed: () {
-          //API Call...
+          _getTwitterData("google", ["public_metrics", "profile_image_url"]);
         },
       ),
       onTap: () {
