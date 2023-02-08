0a0895e10a1bd2c56bfbac06ecee9c6263dfef64
vcalomi <vcalomino@fi.uba.ar>
2023-02-08 20:15:55 -0300

    Added share allocation widget

--- lib/company/company_list_page.dart
+++ lib/company/company_list_page.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fairsite/company/asset/ShareAllocationWidget.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -55,6 +56,7 @@ class CompanyListPage extends ConsumerWidget {
                         : CompanyDetails(
                             ref.watch(activeList)!, selectedItem.notifier),
                   ),
+                  Expanded(child: ShareAllocationWidget())
                   // Expanded(
                   //     child: Card(
                   //         child: Column(
