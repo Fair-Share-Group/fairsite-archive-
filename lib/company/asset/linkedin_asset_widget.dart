22d5ebde6c1a279d681aeaba0a4f93f8f727aa8b
vcalomi <vcalomino@fi.uba.ar>
2023-02-10 12:29:24 -0300

    Fixed linkedin api follower count

--- lib/company/asset/linkedin_asset_widget.dart
+++ lib/company/asset/linkedin_asset_widget.dart
@@ -16,22 +16,27 @@ class LinkedInAssetWidget extends ConsumerWidget {
 
   static const AssetType _type = AssetType.LinkedIn;
 
-
   LinkedInAssetWidget(this.asset);
 
   Future<String> _getLinkedinData(String id) async {
     final keyDoc = await FirebaseFirestore.instance.doc('api/rapidApi').get();
     final response = await http.post(
         Uri.parse(
-            'https://linkedin-company-data.p.rapidapi.com/linkedInCompanyDataJsonV3Beta?liUrl=${getAssetUrl(AssetType.LinkedIn, id)}'),
+            'https://linkedin-company-data.p.rapidapi.com/linkedInCompanyDataJson'),
         headers: {
           'content-type': 'application/json',
           'X-RapidAPI-Key': keyDoc.get('key'),
           'X-RapidAPI-Host': 'linkedin-company-data.p.rapidapi.com'
-        }); //Example data (should use the company linkedin url in the uri.parse and the correct API key)
+        },
+        body: json.encode({
+          "liUrls": [getAssetUrl(AssetType.LinkedIn, id)]
+        }));
     if (response.statusCode == 200) {
       var body = jsonDecode(response.body);
-      await asset.update({"followers": (body['results']['followerCount'] ?? 'Could not find follower count')});
+      await asset.update({
+        "followers": (body['Results'][0]['FollowerCount'] ??
+            'Could not find follower count')
+      });
       return _linkedinData = response.body;
     } else {
       throw Exception("No data found");
@@ -39,26 +44,39 @@ class LinkedInAssetWidget extends ConsumerWidget {
   }
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) => ref.watch(docSP(asset.path)).when(
-    loading: () => Container(), 
-    error: (e, s) => ErrorWidget(e), 
-    data: (assetDoc) => Card(
-      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        ListTile(
-      title: Text('${AssetType.LinkedIn.name} - ${data(assetDoc, 'id')}'),
-      subtitle: Text("followers: ${data(assetDoc, 'followers')}"),
-      trailing: IconButton(
-        icon: const Icon(Icons.refresh),
-        onPressed: () => _getLinkedinData(data(assetDoc, 'id')),
-      ),
-      ),
-      Padding(padding: const EdgeInsets.only(left: 15, bottom: 15), child: ActionChip(
-            avatar: const Icon(Icons.open_in_new_rounded, color: Colors.black26, size: 18,),
-            label: Text("${getAssetUrl(_type, data(assetDoc, 'id'))}"),
-            onPressed: () => openAssestWebpage(_type, data(assetDoc, 'id'), context),
-            ),
-      ),
-      ]),
-    ) 
-    );
+  Widget build(BuildContext context, WidgetRef ref) =>
+      ref.watch(docSP(asset.path)).when(
+          loading: () => Container(),
+          error: (e, s) => ErrorWidget(e),
+          data: (assetDoc) => Card(
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ListTile(
+                        title: Text(
+                            '${AssetType.LinkedIn.name} - ${data(assetDoc, 'id')}'),
+                        subtitle:
+                            Text("followers: ${data(assetDoc, 'followers')}"),
+                        trailing: IconButton(
+                          icon: const Icon(Icons.refresh),
+                          onPressed: () =>
+                              _getLinkedinData(data(assetDoc, 'id')),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 15, bottom: 15),
+                        child: ActionChip(
+                          avatar: const Icon(
+                            Icons.open_in_new_rounded,
+                            color: Colors.black26,
+                            size: 18,
+                          ),
+                          label: Text(
+                              "${getAssetUrl(_type, data(assetDoc, 'id'))}"),
+                          onPressed: () => openAssestWebpage(
+                              _type, data(assetDoc, 'id'), context),
+                        ),
+                      ),
+                    ]),
+              ));
 }
