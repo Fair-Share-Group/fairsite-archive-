ecf0e4a907590cedb0f6d0cd7b32cb47484ea4ed
kovarny <sivochkin@gmail.com>
2023-03-04 12:39:11 +1100

    mobile_first integration

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:fairsite/company/company_list_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobile_first/frame.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fairsite/login_page.dart';
 import 'package:fairsite/dashboard/dashboard_page.dart';
@@ -77,38 +78,84 @@ class TheAppState extends ConsumerState<TheApp> {
         ),
       );
     } else {
-      return Scaffold(
-          body: ref.watch(isLoggedIn) == false
-              ? LoginPage()
-              : DefaultTabController(
-                  initialIndex: 0,
-                  length: 1,
-                  child: Navigator(
-                    onGenerateRoute: (RouteSettings settings) {
-                      // print('onGenerateRoute: ${settings}');
-                      if (settings.name == '/' || settings.name == 'search') {
-                        return PageRouteBuilder(
-                            pageBuilder: (_, __, ___) =>
-                                //DashboardPage()
-                                CompanyListPage());
-                      } else {
-                        //  else if (settings.name == 'lists') {
-                        //   return PageRouteBuilder(
-                        //       pageBuilder: (_, __, ___) => ListsPage());
-                        // } else if (settings.name == 'pep admin') {
-                        //   return PageRouteBuilder(
-                        //       pageBuilder: (_, __, ___) => PepAdminPage());
-                        // } else if (settings.name == 'pep library') {
-                        //   return PageRouteBuilder(
-                        //       pageBuilder: (_, __, ___) => PepLibraryPage());
-                        // } else if (settings.name == 'adverse media') {
-                        //   return PageRouteBuilder(
-                        //       pageBuilder: (_, __, ___) => AdverseMediaPage());
-                        // } else {
-                        throw 'no page to show';
-                      }
-                    },
-                  )));
+      return Stack(
+        children: [
+          Opacity(
+              opacity: 0.5, // set the opacity value between 0.0 and 1.0
+              child: Image(
+                  image: NetworkImage(
+                      'https://images.unsplash.com/photo-1502101872923-d48509bff386?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80'))),
+          Opacity(
+              opacity: 0.8, // set the opacity value between 0.0 and 1.0
+              child: Center(
+                  child: SizedBox(
+                      width: 800,
+                      child: Frame(
+                        onGenerateRoute: (RouteSettings r) {
+                          print('onGenerateRoute: ${r}');
+                          if (r.name == '/') {
+                            return PageRouteBuilder(
+                                pageBuilder: (context, a, b) => LoginPage(),
+                                settings: r);
+                          } else if (r.name == '/blank') {
+                            return PageRouteBuilder(
+                                pageBuilder: (context, a, b) => LoginPage(),
+                                settings: r);
+                          } else if (r.name == '/details') {
+                            return PageRouteBuilder(
+                                pageBuilder: (context, a, b) => LoginPage(),
+                                settings: r);
+                          }
+                          // // return homePage;
+                          // return PageRouteBuilder(
+                          //     pageBuilder: (context, a, b) => HomePage1(), settings: r);
+                        },
+                        // count: 3,
+                        // homePage: PageRouteBuilder(
+                        //     pageBuilder: (context, a, b) =>
+                        //         HomePage(navigatorKeys[1]),
+                        //     settings: RouteSettings(name: '/'),
+                        //     fullscreenDialog: true),
+                        // blankPage: PageRouteBuilder(
+                        //     pageBuilder: (context, a, b) => BlankPage(),
+                        //     fullscreenDialog: true,
+                        //     settings: RouteSettings(name: '/blank')),
+                      ))))
+        ],
+      );
     }
+    //   Scaffold(
+    //       body: ref.watch(isLoggedIn) == false
+    //           ? LoginPage()
+    //           : DefaultTabController(
+    //               initialIndex: 0,
+    //               length: 1,
+    //               child: Navigator(
+    //                 onGenerateRoute: (RouteSettings settings) {
+    //                   // print('onGenerateRoute: ${settings}');
+    //                   if (settings.name == '/' || settings.name == 'search') {
+    //                     return PageRouteBuilder(
+    //                         pageBuilder: (_, __, ___) =>
+    //                             //DashboardPage()
+    //                             CompanyListPage());
+    //                   } else {
+    //                     //  else if (settings.name == 'lists') {
+    //                     //   return PageRouteBuilder(
+    //                     //       pageBuilder: (_, __, ___) => ListsPage());
+    //                     // } else if (settings.name == 'pep admin') {
+    //                     //   return PageRouteBuilder(
+    //                     //       pageBuilder: (_, __, ___) => PepAdminPage());
+    //                     // } else if (settings.name == 'pep library') {
+    //                     //   return PageRouteBuilder(
+    //                     //       pageBuilder: (_, __, ___) => PepLibraryPage());
+    //                     // } else if (settings.name == 'adverse media') {
+    //                     //   return PageRouteBuilder(
+    //                     //       pageBuilder: (_, __, ___) => AdverseMediaPage());
+    //                     // } else {
+    //                     throw 'no page to show';
+    //                   }
+    //                 },
+    //               )));
+    // }
   }
 }
