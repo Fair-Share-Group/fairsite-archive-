3846f5cb6de5d65ce4473573704c7b77be672b23
kovarny <sivochkin@gmail.com>
2023-03-11 13:00:22 +1100

    dep changes

--- lib/company/asset/facebook_asset_widget.dart
+++ lib/company/asset/facebook_asset_widget.dart
@@ -12,35 +12,49 @@ class FacebookAssetWidget extends ConsumerWidget {
   FacebookAssetWidget(this.asset);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) => ref.watch(docSP(asset.path)).when(
-    loading: () => Container(), 
-    error: (e, s) => ErrorWidget(e), 
-    data: (assetDoc) => Card(
-      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        Row(mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-          Padding(padding: const EdgeInsets.only(left: 15), child: 
-            SvgPicture.asset('assets/svg/facebook.svg', width: 36, height: 36,),
-          ),
-          Expanded(child: ListTile(
-            title: Text(_type.name),
-            subtitle: Text(data(assetDoc, 'id')),
-            trailing: IconButton(
-              icon: const Icon(Icons.refresh),
-              onPressed: () {},
-              ),)
-          )
-      ])
-        ,
-      Padding(padding: const EdgeInsets.only(left: 15, bottom: 15), child: ActionChip(
-            avatar: const Icon(Icons.open_in_new_rounded, color: Colors.black26, size: 18,),
-            label: Text("${getAssetDisplayUrl(_type, data(assetDoc, 'id'))}"),
-            onPressed: () => openAssetWebpage(_type, data(assetDoc, 'id'), context),
-            ),
-      ),
-      ]),
-    ) 
-    );
-
-
+  Widget build(BuildContext context, WidgetRef ref) =>
+      ref.watch(docSP(asset.path)).when(
+          loading: () => Container(),
+          error: (e, s) => ErrorWidget(e),
+          data: (assetDoc) => Card(
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(left: 15),
+                              child: SvgPicture.asset(
+                                'assets/svg/facebook.svg',
+                                width: 36,
+                                height: 36,
+                              ),
+                            ),
+                            Expanded(
+                                child: ListTile(
+                              title: Text(_type.name),
+                              subtitle: Text(data(assetDoc, 'id')),
+                              trailing: IconButton(
+                                icon: const Icon(Icons.refresh),
+                                onPressed: () {},
+                              ),
+                            ))
+                          ]),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 15, bottom: 15),
+                        child: ActionChip(
+                          avatar: const Icon(
+                            Icons.open_in_new_rounded,
+                            color: Colors.black26,
+                            size: 18,
+                          ),
+                          label: Text(
+                              "${getAssetDisplayUrl(_type, data(assetDoc, 'id'))}"),
+                          onPressed: () => openAssetWebpage(
+                              _type, data(assetDoc, 'id'), context),
+                        ),
+                      ),
+                    ]),
+              ));
 }
